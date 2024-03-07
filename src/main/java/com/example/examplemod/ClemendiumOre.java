274ebc37500318bb7bdb8c731700c05075cff201
Curtis Jones <curtisclajones@gmail.com>
2024-03-07 10:16:55 +0100

    Initial State

---
+++ src/main/java/com/example/examplemod/ClemendiumOre.java
@@ -0,0 +1,10 @@
+package com.example.examplemod;
+
+import net.minecraft.world.level.block.Block;
+
+public class ClemendiumOre extends  {
+
+    public ClemendiumOre() {
+        super();
+    }
+}
